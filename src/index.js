bba8a05560ebcad860f253278d5e37c409ea4474
Ulyana Zhivotenkova <ulyanazh2@gmail.com>
2020-09-29 18:10:32 +0400

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length === 0) {
+       return 0;
+    }
+    function compareNumber (a,b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+        }
+       array = array.sort(compareNumber);
+       return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length === 0) {
+        return 0;
+    }
+    function compareNumber (a,b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+        }
+       array = array.sort(compareNumber);
+       return array[array.length - 1]; 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length === 0) {
+        return 0;
+    }
+  let total = 0;
+  for(let i=0; i < array.length; i++) {
+    total += array[i];
+  }
+  return avgNumber = total/array.length;
 }
